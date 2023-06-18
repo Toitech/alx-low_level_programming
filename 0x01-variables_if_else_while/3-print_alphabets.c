4ecc022de98722195e48746437fac4a92f2b6720
Toitech <ajagbeoluwadarasimifourjesus@gmail.com>
2023-06-18 01:49:39 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+{	char alp[26] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 26; i++)
